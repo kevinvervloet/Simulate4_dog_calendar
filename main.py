5032bc05f711e3bb16e97209d6c3c0865a3d2adc
KevinKDG <72076173+KevinKDG@users.noreply.github.com>
2022-02-13 19:14:35 +0100

    Starting Design App

--- main.py
+++ main.py
@@ -1,14 +1,58 @@
+#!/usr/bin/env python
+
+"""
+Python App
+"""
+
+#               IMPORTS               #
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.app import MDApp
+from kivy.lang import Builder
 
-kivy.require('1.11.1')
+#          AUTHOR INFORMATION         #
+
+#        _____
+#      .'     `.
+#     /  .-=-.  \   \ __
+#     | (  C\ \  \_.'')
+#     _\  `--' |,'   _/
+#    _/__`.____.'__.-' The coding snail~
+
+__author__ = "Kevin Vervloet"
+__email__ = "kevin.vervloet@student.kdg.be"
+__Version__ = "1.0"
+__status__ = "Development"
+
+#              MAIN CODE              #
 
 
-# Defining a class
+class WindowManager(ScreenManager):
+    pass
+
+
+class CalendarScreen(Screen):
+    pass
+
+
+class StartScreen(Screen):
+    pass
+
+kv = Builder.load_file('dogcalendar.kv')
+
+
 class DogCalendar(App):
     def build(self):
-        return Label(text="Hello World !")
+        return kv
 
 
-DogCalendar().run()
+if __name__ == '__main__':
+    DogCalendar().run()
