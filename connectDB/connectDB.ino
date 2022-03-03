29b04bc7f4760ce2c4f9061aacc48349f17bd23e
Kevin Vervloet <72076173+KevinKDG@users.noreply.github.com>
2022-03-03 13:11:41 +0100

    Connecting to Firebase with arduino

---
+++ connectDB/connectDB.ino
@@ -0,0 +1,61 @@
+/* Documentation firebase library for arduino https://github.com/mobizt/Firebase-Arduino-WiFiNINA for ESP32 
+ * 
+ * 
+ * 
+*/
+
+#include "Firebase_Arduino_WiFiNINA.h"
+
+// configure wifi & firebase settings
+#define DATABASE_URL "https://pet-friend-29364-default-rtdb.europe-west1.firebasedatabase.app/" //pet-friend-29364-default-rtdb.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_SECRET "Oe87LHxIL5Ep9R0lTDPqFm3xP80jRtDYsqAZnnlH"
+#define WIFI_SSID "WiFi-5.0-133B"               //IoT
+#define WIFI_PASSWORD "24652CC8A4"          //KdGIoT17!"
+
+//Define Firebase data object
+FirebaseData fbdo;
+int val = 0;
+
+// Setup
+void setup()
+{
+  Serial.begin(115200);
+  delay(100);
+  Serial.println();
+  Serial.print("Connecting to Wi-Fi");
+  int status = WL_IDLE_STATUS;
+  while (status != WL_CONNECTED)
+  {
+    status = WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("Connected to WiFi");
+  Serial.println();
+
+  //Provide the autntication data
+  Firebase.begin(DATABASE_URL, DATABASE_SECRET, WIFI_SSID, WIFI_PASSWORD);
+  Firebase.reconnectWiFi(true);
+
+  String path = "/km/km";
+
+  Serial.println();
+
+  Serial.println("Getting value from /km/km database... ");
+  
+
+  if (Firebase.get(fbdo, path)) {
+    Serial.println("km: " + fbdo.stringData());
+  }
+  else
+  {
+    Serial.println("error, " + fbdo.errorReason());
+  }
+}
+
+void loop()
+{
+}
